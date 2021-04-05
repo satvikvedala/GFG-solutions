86566baf79836ede96d892aa3423cbeb337e9620
satvikvedala <72596484+satvikvedala@users.noreply.github.com>
2021-04-05 20:33:55 +0530

    Update LinkedLists.py

--- LinkedLists.py
+++ LinkedLists.py
@@ -34,3 +34,37 @@ def flatten(root):
         tan.bottom = Node(res[y])
         tan = tan.bottom
     return head
+
+#Flattening a Linked List(Long approach) JAVA
+class GfG
+{
+    Node merge(Node a, Node b){
+        if(a == null){
+            return b;
+        }
+        if(b == null){
+            return a;
+        }
+        Node result;
+        if(a.data<b.data){
+            result = a;
+            result.bottom = merge(a.bottom,b);
+        }
+        else{
+            result = b;
+            result.bottom = merge(a,b.bottom);
+        }
+        result.next = null;
+        return result;
+    }
+    Node flatten(Node root)
+    {
+	// Your code here
+	if (root == null || root.next == null){
+	    return root;
+	}
+	root.next = flatten(root.next);
+	root = merge(root,root.next);
+	return root;
+    }
+}
