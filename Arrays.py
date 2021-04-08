69febd9555cbcf8154113b9920f8fbb36b10b839
satvikvedala <72596484+satvikvedala@users.noreply.github.com>
2021-04-08 18:52:24 +0530

    Update Arrays.py

--- Arrays.py
+++ Arrays.py
@@ -1,3 +1,7 @@
+  #Missing number in array
+  def MissingNumber(array,n):
+    # code here
+    return (n*(n+1)//2) - sum(array)
   #MergeSort  
   def merge(self,arr, l, m, r): 
         # code here
