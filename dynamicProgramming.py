e2bebd4df3386e3639c33b5bd4da337206c4f628
satvikvedala <72596484+satvikvedala@users.noreply.github.com>
2021-04-17 10:55:30 +0530

    Update dynamicProgramming.py

--- dynamicProgramming.py
+++ dynamicProgramming.py
@@ -16,3 +16,22 @@
                 else:
                     continue
         return False
+
+#Unique BST's (Recursive Approach) Time limit High
+    def numTrees(self,N):
+        # code here
+        if N == 0:
+            return 1
+        su = 0
+        for i in range(N):
+            su+=self.numTrees(i)*self.numTrees(N-i-1)
+        return su%1000000007
+#Unique BST's(DP approach)
+    def numTrees(self,N):
+        # code here
+        dp = [0]*(n+1)
+        dp[0] = 1
+        for i in range(1,n+1):
+            for j in range(i):
+                dp[i] += dp[j]*dp[i-j-1]
+        return (dp[n]%1000000007)
