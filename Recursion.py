25314b373c35dc83cbf1bb4e1941d815789b8755
satvikvedala <72596484+satvikvedala@users.noreply.github.com>
2021-04-18 09:57:21 +0530

    Create Recursion.py

---
+++ Recursion.py
@@ -0,0 +1,11 @@
+#Partition Equal Subset Sum
+        def func(N,arr,index,curr_sum,rem_sum):
+            if (curr_sum>rem_sum) or (index>=N):
+                return False
+            if curr_sum == rem_sum:
+                return True
+            return func(N,arr,index+1,curr_sum + arr[index],rem_sum) or func(N,arr,index+1,curr_sum,rem_sum) 
+        rem_sum = sum(arr)
+        if rem_sum%2!=0:
+            return False
+        return func(N,arr,0,0,rem_sum//2)
