7d805478aade50f81a86d50d357393af4778e6de
satvikvedala <72596484+satvikvedala@users.noreply.github.com>
2021-04-29 11:43:34 +0530

    Create Substrings.py

---
+++ Strings/Substrings.py
@@ -0,0 +1,13 @@
+class Solution:
+	def AllPossibleStrings(self, s):
+		# Code here
+		def func(s,arr,st,n,i):
+		    if n == 0:
+		        arr.append(st[::-1])
+		        return 
+		    func(s,arr,st,n-1,i-1)
+		    func(s,arr,st+s[i],n-1,i-1)
+		arr = []
+		st = ''
+		func(s,arr,st,len(s),len(s)-1)
+		return arr[1:]
