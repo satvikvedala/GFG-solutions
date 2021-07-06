e13c1ef52b1025fd1f99352ef95d9e1cfc3f092f
satvikvedala <72596484+satvikvedala@users.noreply.github.com>
2021-07-06 15:11:05 +0530

    Create Longest Palindromic Subsequence.py

---
+++ Strings/Longest Palindromic Subsequence.py
@@ -0,0 +1,17 @@
+class Solution:
+
+    def longestPalinSubseq(self, S):
+        # code here
+        s = S[::-1]
+        dp = [[0 for j in range(len(s)+1)] for i in range(len(S)+1)]
+        for i in range(len(S)+1):
+            dp[i][0] = 0
+        for j in range(len(s)+1):
+            dp[0][j] = 0
+        for i in range(1,len(S)+1):
+            for j in range(1,len(s)+1):
+                if S[i-1] == s[j-1]:
+                    dp[i][j] = 1+dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+        return dp[len(S)][len(s)]
